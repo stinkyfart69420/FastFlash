a08cc5ce015b91dfe73300e95dd96b9f95ec08fe
stinkyfart69420 <147311474+stinkyfart69420@users.noreply.github.com>
2024-03-31 09:16:49 +0300

    Removed —noupdate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import os
-import sys
 
-if len(sys.argv) > 1:
-            if sys.argv[1]== "--noupdate":
-                os.system("cd update && python3 main.py")
-exit()
 
 print("Downloading latest version...")
 os.system("git clone https://github.com/stinkyfart69420/FastFlash")
