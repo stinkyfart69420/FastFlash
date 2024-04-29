768fc7e486165c7567df35219e6576b00aeb3077
stinkyfart69420 <147311474+stinkyfart69420@users.noreply.github.com>
2024-04-29 09:32:12 +0300

    Removed auto updater

--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.system("sudo apt install git")
-print("Downloading latest version...")
-os.system("git clone https://github.com/stinkyfart69420/FastFlash")
-print("Downloaded succesfully. Launching FastFlash...")
-os.system("cd FastFlash && cd update && python3 main.py")
-print("Cleaning up before exiting...")
-os.system("rm -rf FastFlash")
