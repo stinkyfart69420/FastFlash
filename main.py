ff80325181170c0959b46f5e8c51138a6550decb
stinkyfart69420 <147311474+stinkyfart69420@users.noreply.github.com>
2024-04-29 09:08:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 
-
+os.system("sudo apt install git")
 print("Downloading latest version...")
-os.system("nohup git clone https://github.com/stinkyfart69420/FastFlash")
+os.system("git clone https://github.com/stinkyfart69420/FastFlash")
 print("Downloaded succesfully. Launching FastFlash...")
 os.system("cd FastFlash && cd update && python3 main.py")
 print("Cleaning up before exiting...")
